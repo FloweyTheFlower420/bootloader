0b8e34a1f5145a3887b322b56242f5f565714368
FloweyTheFlower420 <guywithanaxe42@gmail.com>
2022-01-15 22:34:58 -0600

    working on romfont + term support

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-This is a cesspool of random bits of code required to get into kernel. I will not, and can not fix this.
 # bootloader
+Go use limine instead lol
--- stage2/utils.h
+++
@@ -1,8 +0,0 @@
-#ifndef __STAGE2_UTILS_H__
-#define __STAGE2_UTILS_H__
-#include <cstddef>
-
-void check_for_long_mode();
-void panic(const char* str);
-void* memcpy(void*, void*, size_t);
-#endif
